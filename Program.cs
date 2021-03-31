f1deb0d878bb871a006d82e7095f8dedffbf75c1
jimii88 <nguyen.tat.thanh@miyatsu.vn>
2021-03-31 15:33:40 +0700

    using sqlhelper

--- Program.cs
+++ Program.cs
@@ -8,13 +8,35 @@ namespace SqlHelpers
 {
     static class Program
     {
+        public static string connectionString = string.Format("Data Source={0};Initial Catalog={1};Persist Security Info=True;User ID={2};Password={3};Connect Timeout={4};", "thanhnt", "Wii", "sa", "@dmin123", 30);
+
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-           
+            Select();
+        }
+
+        private static void Select()
+        {
+            string sqlString = "select [プロジェクトID] , [機能ID], [機能名], [タイムスタンプ] FROM [Wii].[dbo].[Fes機能ID] where [プロジェクトID] = @プロジェクトID AND [機能ID] = @機能ID";
+
+            Parameters parameter = new Parameters();
+            parameter.Add(new Parameter()
+            {
+                Name = "@プロジェクトID",
+                Values =";delete * from [Wii].[dbo].[Fes機能ID] --"
+            });
+            parameter.Add(new Parameter()
+            {
+                Name = "@機能ID",
+                Values = 110
+            });
+
+            var dt = SqlHelpers.ExecuteDataset(connectionString, System.Data.CommandType.Text, sqlString, parameter);
         }
     }
 }
