bf33599d3deb7d1c1e1f5a21eec9c9af038f6898
jimii88 <nguyen.tat.thanh@miyatsu.vn>
2022-03-18 10:41:26 +0700

    create new class connectionInfo

--- Program.cs
+++ Program.cs
@@ -1,27 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace SqlHelpers
 {
     static class Program
-    {
-        public static string connectionString = string.Format("Data Source={0};Initial Catalog={1};Persist Security Info=True;User ID={2};Password={3};Connect Timeout={4};", "thanhnt", "Wii", "sa", "@dmin123", 30);
-
-
+    {  
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
-        {
+        {            
+
             Select();
         }
 
         private static void Select()
         {
+            ConnectionInfo connectionInfo = new ConnectionInfo()
+            {
+                ServerName ="THANHPC",
+                UserName="sa",
+                Password= "@dmin123",
+                DatabaseName= "Wii",
+                TimeOutCommand = 300,
+                TimeOutConnection = 20
+            };
+
+            SqlHelpers.CreateConnectionString(connectionInfo);
+
             string sqlString = "select [プロジェクトID] , [機能ID], [機能名], [タイムスタンプ] FROM [Wii].[dbo].[Fes機能ID] where [プロジェクトID] = @プロジェクトID AND [機能ID] = @機能ID";
 
             Parameters parameter = new Parameters();
@@ -37,7 +43,7 @@ namespace SqlHelpers
             });
             
 
-            var dt = SqlHelpers.ExecuteDataset(connectionString, System.Data.CommandType.Text, sqlString, parameter);
+            var dt = SqlHelpers.ExecuteDataset(SqlHelpers.ConnectionString, System.Data.CommandType.Text, sqlString, parameter);
         }
     }
 }
